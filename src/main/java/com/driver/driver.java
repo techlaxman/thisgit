95df9110a59a26193f9c87c468da8f899a53f3fb
more-laxman000 <morelaxman666@gmail.com>
2023-12-21 12:08:21 +0530

    tttt

--- src/main/java/com/driver/driver.java
+++ src/main/java/com/driver/driver.java
@@ -15,27 +15,15 @@ public class driver {
 		System.out.println("test 1 executed..");
 		System.out.println("test 1 executed..");
 		System.out.println("test 1 executed..");
-		
 		System.out.println("Laxman.. new branch is created..");
-		
 		System.out.println("test 1");
-<<<<<<< HEAD
-=======
-
->>>>>>> f5db8cae7ac30e933acd5a9cded31abbed5d85e3
-		
 		System.out.println("Laxman");
-		
 		System.out.println("llaxman branch commit");
-		
 		System.out.println("more branch created...");
-		
 		System.out.println("merge changes....");
-<<<<<<< HEAD
-		
 		System.out.println("more");
-=======
 
->>>>>>> f5db8cae7ac30e933acd5a9cded31abbed5d85e3
+
+
 	}
 }
