0c8dddb8171e8eb72b727097b7852e9a32c4a30b
more-laxman000 <morelaxman666@gmail.com>
2024-01-23 12:32:14 +0530

    fff

---
+++ src/main/java/com/driver/driver.java
@@ -0,0 +1,25 @@
+package com.driver;
+
+import org.testng.annotations.Test;
+
+public class driver {
+
+	
+	@Test
+	public void test1() {
+		
+		System.out.println("test 1 executed..");
+		System.out.println("test 1 executed..");
+		System.out.println("test 1 executed..");
+		System.out.println("test 1 executed..");
+		System.out.println("test 1 executed..");
+		System.out.println("test 1 executed..");
+		System.out.println("test 1 executed..");
+		
+		System.out.println("Laxman.. new branch is created..");
+		
+		System.out.println("test 1");
+		
+		System.out.println("Laxman");
+	}
+}
